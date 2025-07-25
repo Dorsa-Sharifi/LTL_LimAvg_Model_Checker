2d760ae110d85d69167b4be1b968b5cd96067036
Dorsa-Sharifi <dorsa.sharifi@gmail.com>
2025-07-25 09:26:48 +0330

    Clearing Code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+#TODO: Testing Kripke Structure
 from model import KripkeStructure, kripke_parser
-
-
 def parse_kripke(input_text):
     try:
         return kripke_parser.parse(input_text)
@@ -35,128 +34,41 @@ if __name__ == "__main__":
     else:
         print("Failed to parse input")
 
+#TODO: Testing LTL Parser
+from ltl import LTLimModelChecker
+from ltl.parser import LTLParser
+from ltl.visualizer import ParseTreeVisualizer
+import os
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # # from ltl import LTLimModelChecker
-# # # from ltl.parser import LTLParser
-# # # from ltl.visualizer import ParseTreeVisualizer
-# # # import os
 # #
-# #
-# # # def test_formula(formula):
-# # #     parser = LTLParser()
-# # #     visualizer = ParseTreeVisualizer()
-# # #
-# # #     try:
-# # #         print(f"\nFormula: {formula}")
-# # #
-# # #         # Create safe filename
-# # #         safe_name = "".join(c if c.isalnum() else "_" for c in formula)[:20]
-# # #
-# # #         # Parse original
-# # #         tree = parser.parse(formula)
-# # #         print("Original:", tree)
-# # #         visualizer.visualize(tree, f"original_{safe_name}.png")
-# # #
-# # #         # Parse negated
-# # #         negated = parser.negate(tree)
-# # #         print("Negated:", negated)
-# # #         visualizer.visualize(negated, f"negated_{safe_name}.png")
-# # #
-# # #     except Exception as e:
-# # #         print(f"Error: {str(e)}")
-# #
-# #
-# # # if __name__ == "__main__":
-# # #     # test_formula("G(p || LimSupAvg(u))")
-# # #     # test_formula("F(p && Xq)")
-# # #     # test_formula("G(p -> Fq)")
-# # #     # test_formula("(p U q) && (r || s)")
-# # #     # test_formula("!(Xp || Gq)")
-# # #
-# # #     parser = LTLParser()
-# # #     visualizer = ParseTreeVisualizer()
-# # #
-# # #     # Test with various formulas
-# # #     formulas = [
-# # #         "G(p || LimSupAvg(u))",
-# # #         "F(a && X b)",
-# # #         "LimInfAvg(x) U G(y)"
-# # #     ]
-# # #
-# # #         # Example with multiple formulas
-# # #         input_text = """
-# # #         states = {s1, s2, s3}
-# # #         initstate = s1
-# # #         edges = {(s1, s2), (s1, s3)}
-# # #         logicalformulas = {s1: {p, q}, s2: {p, r}, s3: {t}}
-# # #         v_s1 = 3, v_s2 = -5, v_s3 = 1
-# # #         """
-# # #
-# # #         parsed_data = parse_input(input_text)
-# # #         print("Parsed Data:", parsed_data)
-# # #
-# # #         graph = StateTransitionGraph()
-# # #         graph.build_from_parsed_data(parsed_data)
-# #
-# #     # for formula in formulas:
-# #     #     parsed = parser.parse(formula)
-# #     #     print(f"Visualizing: {formula}")
-# #     #     visualizer.visualize(parsed)
-#
-#
+def test_formula(formula):
+    parser = LTLParser()
+    visualizer = ParseTreeVisualizer()
+
+    try:
+        print(f"\nFormula: {formula}")
+
+        # Create safe filename
+        safe_name = "".join(c if c.isalnum() else "_" for c in formula)[:20]
+
+        # Parse original
+        tree = parser.parse(formula)
+        print("Original:", tree)
+        visualizer.visualize(tree, f"original_{safe_name}.png")
+
+        # Parse negated
+        negated = parser.negate(tree)
+        print("Negated:", negated)
+        visualizer.visualize(negated, f"negated_{safe_name}.png")
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+
+if __name__ == "__main__":
+    test_formula("G(p || LimSupAvg(u))")
+    test_formula("F(p && Xq)")
+    test_formula("G(p -> Fq)")
+    test_formula("(p U q) && (r || s)")
+    test_formula("!(Xp || Gq)")
+
